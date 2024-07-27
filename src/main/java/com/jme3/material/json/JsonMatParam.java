e4a35b8e9f30e2c0cadee95573fe8af27e151296
capdevon <jefferydeaver2010@gmail.com>
2024-07-27 22:12:57 +0200

    @JsonInclude(JsonInclude.Include.NON_NULL)

--- src/main/java/com/jme3/material/json/JsonMatParam.java
+++ src/main/java/com/jme3/material/json/JsonMatParam.java
@@ -1,7 +1,9 @@
 package com.jme3.material.json;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.jme3.texture.image.ColorSpace;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class JsonMatParam {
     
     public String name;
@@ -14,6 +16,7 @@ public class JsonMatParam {
         return "MaterialParameter [name=" + name 
                 + ", value=" + value 
                 + ", texture=" + texture 
+                + ", colorSpace=" + colorSpace
                 + "]";
     }
 
--- src/main/java/com/jme3/material/json/JsonTexture.java
+++ src/main/java/com/jme3/material/json/JsonTexture.java
@@ -1,13 +1,15 @@
 package com.jme3.material.json;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.jme3.texture.Texture.MagFilter;
 import com.jme3.texture.Texture.MinFilter;
 import com.jme3.texture.Texture.WrapMode;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class JsonTexture {
     
     public String path;
-    public boolean flipY;
+    public Boolean flipY;
     public WrapMode wrapMode;
     public MinFilter minFilter;
     public MagFilter magFilter;
